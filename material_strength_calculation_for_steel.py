3cff82151caef718ef7fb39932696aab22270d63
Ma. Womah S. Montejo amd Denise Laureen Paredes <deniselaureen16@gmail.com>
2025-03-26 18:40:18 +0800

    Add `store_calculator.py` program.

--- material_strength_calculation_for_steel.py
+++ material_strength_calculation_for_steel.py
@@ -2,12 +2,12 @@
 # Material Strength Calculation for Steel
 
 def material_strength_calculation_for_steel():
-    force = float(input("Enter the force applied to the beam (N):"))
-    area = float(input(Enter the cross-sectional area of the beam (mm^2):"))
+    F = float(input("Enter the force applied to the beam (N):"))
+    A = float(input("Enter the cross-sectional area of the beam (mm^2):")) 
     
-    stress = material_strength(force, area) 
+    stress = F/A
     
-    print(f"So, the stress on the beam is {stress:.2f}Pa.\n")
+    print(f"So, the stress on the beam is {stress:.2f}Pa.\n") 
     
 if __name__== "__main__":
-    material_strength_calculation_for_steel() 
+    material_strength_calculation_for_steel()     
